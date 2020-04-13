7421a520725b69a85cbc48f3883fcec8a139d391
wo2jin <w2jin2732@naver.com>
2020-04-13 17:46:18 +0900

    0413

--- JavaTest4/src/com/test/Test1.java
+++ JavaTest4/src/com/test/Test1.java
@@ -26,7 +26,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 		Dog d = new Dog();
-		d.
+
 		
 	}
 
---
+++ JavaTest4/src/com/test/Test10.java
@@ -0,0 +1,19 @@
+package com.test;
+
+class MyClass4{
+	static int a;
+	static int b;
+	static {
+		a=1;
+		b=2;
+		
+	}
+}
+public class Test10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
